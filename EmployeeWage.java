db228ff60e77c53282aaa61df7f81707b5312e40
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-07 13:17:46 +0530

    add: isPresent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,25 @@
 package Day10_EmployeeWage;
 
-public class EmployeeWage {
+import java.util.Random;
 
+public class EmployeeWage {
+	static int FULLTIME_HOUR = 8;
+	public static int hours;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 	}
-
+	
+	public static void isPresent() {
+		Random ran = new Random();
+		int isPresent = ran.nextInt(2);
+		switch (isPresent) {
+		case 0:
+			hours = 0;
+			break;
+		case 1:
+			hours = FULLTIME_HOUR;
+			break;
+		}
+	}
 }
